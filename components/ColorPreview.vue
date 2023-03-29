a14c02b11bff792d6391f5b54bb466e60c33ca5d
pyncz <pavelyanycz@gmail.com>
2023-03-29 16:33:44 +0400

    feat: Add colors' preview

---
+++ components/ColorPreview.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="tw-py-1 tw-inline-flex tw-flex-center-y tw-gap-2">
+    <div
+      :style="{ backgroundColor: rgb }"
+      class="tw-size-em tw-rounded-sm tw-border tw-border-dim-2"
+    />
+    <copy-value :value="rgb" class="tw-text-7/8" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { RGB } from '../models'
+
+const props = defineProps<{
+  color: RGB
+}>()
+
+const rgb = computed(() => `rgb(${props.color.join(', ')})`)
+</script>
---
+++ components/PalettePreview.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="childColors" class="tw-py-1/2">
+    <h6 v-if="label" class="tw-mb-1">
+      {{ label }}
+    </h6>
+    <PalettePreview
+      v-for="childColor, index of childColors"
+      :key="index"
+      :colors="childColor"
+      :label="`${label}-${getSemanticName(index, childColors.length, semanticNames)}`"
+      class="tw-ml-1 tw-border-l tw-border-dim-2 tw-pl-3"
+    />
+  </div>
+  <ColorPreview v-else-if="color" :color="color" />
+</template>
+
+<script setup lang="ts">
+import type { RGB } from '../models'
+
+type Color = RGB | Color[]
+
+const props = defineProps<{
+  colors?: Color
+  label?: string
+  semanticNames?: string[]
+}>()
+
+const isColor = computed(() =>
+  props.colors
+  && props.colors.length === 3
+  && (props.colors as any[]).every(channel => typeof channel === 'number'),
+)
+const childColors = computed(() => isColor.value ? null : props.colors as Color[])
+const color = computed(() => isColor.value ? props.colors as RGB : null)
+</script>
