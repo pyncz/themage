157cd540dfecd59aba4aaacaa81f6366b917e0ec
pyncz <pavelyanycz@gmail.com>
2023-03-29 16:33:10 +0400

    fix: Update styles / layout

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,7 +2,7 @@
   <footer class="tw-opacity-muted tw-flex tw-flex-col xs:tw-flex-row tw-gap-2 tw-text-center tw-items-center">
     <nav class="tw-inline-flex tw-gap-2">
       <a
-        class="tw-text-lg tw-inline-flex"
+        class="tw-link-muted tw-text-lg tw-inline-flex"
         :href="`https://twitter.com/${APP_TWITTER_HANDLE}`"
         target="_blank"
         rel="noreferrer noopener"
@@ -10,7 +10,7 @@
         <icon name="twitter" />
       </a>
       <a
-        class="tw-text-lg tw-inline-flex"
+        class="tw-link-muted tw-text-lg tw-inline-flex"
         :href="`https://github.com/${APP_GITHUB_USERNAME}`"
         target="_blank"
         rel="noreferrer noopener"
@@ -18,9 +18,14 @@
         <icon name="github" />
       </a>
     </nav>
-    <span class="tw-text-3 tw-text-xs tw-inline-block">
-      © {{ yearsFrom }}. Made by
-      <a href="https://github.com/pyncz" target="_blank" rel="noreferrer noopener">
+    <span class="tw-text-dim-2 tw-text-xs tw-inline-block">
+      © {{ yearsFrom }}.
+      {{ $t('madeBy') }}
+      <a
+        href="https://github.com/pyncz"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
         pyncz
       </a>
     </span>
--- models/colors.ts
+++
@@ -1,2 +0,0 @@
-export type RGB = [number, number, number]
-export type RGBA = [number, number, number, number]
